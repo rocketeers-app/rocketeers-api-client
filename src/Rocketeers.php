e7063c7ceec0ef0e23300e88168c75a8f9270af5
Mark van Eijk <mark@vaneijk.co>
2020-02-29 16:53:03 +0100

    report

--- src/Rocketeers.php
+++ src/Rocketeers.php
@@ -4,4 +4,15 @@ namespace Rocketeers;
 
 class Rocketeers
 {
+    protected $client;
+
+    public function __construct()
+    {
+        $this->client = '';
+    }
+
+    public function report(array $report)
+    {
+        dd($report);
+    }
 }
