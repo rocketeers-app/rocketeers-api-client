c56c49540c50740a337883a30cc0d182a74ab62a
Mark van Eijk <mark@vaneijk.co>
2020-02-29 17:46:18 +0100

    wip

--- src/Rocketeers.php
+++ src/Rocketeers.php
@@ -7,16 +7,40 @@ use Zttp\Zttp;
 class Rocketeers
 {
     protected $client;
+    protected $baseUrl;
 
     public function __construct($token)
     {
-        $this->client = Zttp::withHeaders([
+        $this->baseUrl = 'https://rocketeers.app/api/v1';
+
+        $this->client = Zttp::withOptions([
+            'verify' => false,
+            'timeout' => 3,
+        ])->withHeaders([
             'Authorization' => 'Bearer ' . $token,
         ]);
     }
 
     public function report(array $report)
     {
-        dd($this->client);
+        return $this->client->post($this->baseUrl . '/errors', [
+            'site_id' => 1,
+            'user_id' => 1,
+            'code' => 500,
+            'method' => 'GET',
+            'name' => $report['message'],
+            'message' => $report['message'],
+            'file' => '',
+            'line' => '',
+            'trace' => '',
+            'url' => 'url',
+            'referrer' => '',
+            'inputs' => '',
+            'files' => '',
+            'user_name' => '',
+            'user_agent' => '',
+            'ip_address' => '',
+            'hostname' => '',
+        ]);
     }
 }
