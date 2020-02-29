6d13b1c5b467b8e974d219a635c85002fb334bd1
Mark van Eijk <mark@vaneijk.co>
2020-02-29 19:42:46 +0100

    report more data

--- src/Rocketeers.php
+++ src/Rocketeers.php
@@ -21,26 +21,34 @@ class Rocketeers
         ]);
     }
 
-    public function report(array $report)
+    public function data(array $data)
     {
         return $this->client->post($this->baseUrl . '/errors', [
             'site_id' => 1,
             'user_id' => 1,
-            'code' => 500,
-            'method' => 'GET',
-            'name' => $report['message'],
-            'message' => $report['message'],
-            'file' => '',
-            'line' => '',
-            'trace' => '',
-            'url' => 'url',
-            'referrer' => '',
-            'inputs' => '',
-            'files' => '',
-            'user_name' => '',
-            'user_agent' => '',
-            'ip_address' => '',
-            'hostname' => '',
+
+            'channel' => $data['channel'],
+            'code' => $data['code'],
+            'context' => $data['context'],
+            'datetime' => $data['datetime'],
+            'exception' => $data['context']['exception'],
+            'extra' => $data['extra'],
+            'level_name' => $data['level_name'],
+            'level' => $data['level'],
+            'message' => $data['message'],
+
+            'method' => $data['method'],
+            'url' => $data['url'],
+            'referrer' => $data['referrer'],
+            'querystring' => $data['querystring'],
+            'ip_address' => $data['ip_address'],
+            'hostname' => $data['hostname'],
+            'user_agent' => $data['user_agent'],
+            'inputs' => $data['inputs'],
+            'inputs' => $data['inputs'],
+            'headers' => $data['headers'],
+            'session' => $data['session'],
+            'cookies' => $data['cookies'],
         ]);
     }
 }
