ee526bfffc426e99e81c635076a4ac083d5e2da0
Mark van Eijk <mark@vaneijk.co>
2020-06-21 13:11:43 +0200

    wip

--- src/Rocketeers.php
+++ src/Rocketeers.php
@@ -24,40 +24,36 @@ class Rocketeers
 
     public function report(array $data)
     {
-        try {
-            return $this->client->post($this->baseUrl.'/errors', [
-                'site_id' => 1,
-                'user_id' => 1,
+        return $this->client->post($this->baseUrl.'/errors', [
+            'site_id' => 1,
+            'user_id' => 1,
 
-                'channel' => $data['channel'],
-                'environment' => $data['environment'],
-                'code' => $data['code'],
-                'context' => $data['context'],
-                'datetime' => $data['datetime'],
-                'exception' => $data['exception'],
-                'extra' => $data['extra'],
-                'file' => $data['file'],
-                'level_name' => $data['level_name'],
-                'level' => $data['level'],
-                'line' => $data['line'],
-                'message' => $data['message'],
-                'trace' => $data['trace'],
+            'channel' => $data['channel'],
+            'environment' => $data['environment'],
+            'code' => $data['code'],
+            'context' => $data['context'],
+            'datetime' => $data['datetime'],
+            'exception' => $data['exception'],
+            'extra' => $data['extra'],
+            'file' => $data['file'],
+            'level_name' => $data['level_name'],
+            'level' => $data['level'],
+            'line' => $data['line'],
+            'message' => $data['message'],
+            'trace' => $data['trace'],
 
-                'cookies' => $data['cookies'],
-                'files' => $data['files'],
-                'headers' => $data['headers'],
-                'hostname' => $data['hostname'],
-                'inputs' => $data['inputs'],
-                'ip_address' => $data['ip_address'],
-                'method' => $data['method'],
-                'querystring' => $data['querystring'],
-                'referrer' => $data['referrer'],
-                'session' => $data['session'],
-                'url' => $data['url'],
-                'user_agent' => $data['user_agent'],
-            ]);
-        } catch (ConnectionException $e) {
-            dd($e);
-        }
+            'cookies' => $data['cookies'],
+            'files' => $data['files'],
+            'headers' => $data['headers'],
+            'hostname' => $data['hostname'],
+            'inputs' => $data['inputs'],
+            'ip_address' => $data['ip_address'],
+            'method' => $data['method'],
+            'querystring' => $data['querystring'],
+            'referrer' => $data['referrer'],
+            'session' => $data['session'],
+            'url' => $data['url'],
+            'user_agent' => $data['user_agent'],
+        ]);
     }
 }
