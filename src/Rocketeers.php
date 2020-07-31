fa0f2a4e0b5caf6fdaa71785e86599950701762c
Mark van Eijk <mark@vaneijk.co>
2020-07-31 14:36:54 +0200

    post data array in full

--- src/Rocketeers.php
+++ src/Rocketeers.php
@@ -24,34 +24,6 @@ class Rocketeers
 
     public function report(array $data)
     {
-        return $this->client->post($this->baseUrl.'/errors', [
-            'channel' => $data['channel'],
-            'environment' => $data['environment'],
-            'code' => $data['code'],
-            'exception' => $data['exception'],
-            'message' => $data['message'],
-            'context' => $data['context'], // not saved currently
-            'datetime' => $data['datetime'], // not saved currently
-            'extra' => $data['extra'], // not saved currently
-            'level' => $data['level'], // not saved currently
-            'level_name' => $data['level_name'], // not saved currently
-            'file' => $data['file'],
-            'line' => $data['line'],
-            'trace' => $data['trace'],
-            'method' => $data['method'],
-            'url' => $data['url'],
-            'querystring' => $data['querystring'],
-            'referrer' => $data['referrer'],
-            'headers' => $data['headers'],
-            'cookies' => $data['cookies'],
-            'files' => $data['files'],
-            'inputs' => $data['inputs'],
-            'sessions' => $data['sessions'],
-            'user_name' => $data['user_name'],
-            'user_agent' => $data['user_agent'],
-            'ip_address' => $data['ip_address'],
-            'hostname' => $data['hostname'],
-            'command' => $data['command'],
-        ]);
+        return $this->client->post($this->baseUrl.'/errors', $data);
     }
 }
