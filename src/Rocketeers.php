3d3109c76b7f25694f4867c00d7c24cc5a3b7cf0
Mathieu <mathieu@vormkracht10.nl>
2021-06-16 10:22:09 +0200

    Use Http client

--- src/Rocketeers.php
+++ src/Rocketeers.php
@@ -2,26 +2,27 @@
 
 namespace Rocketeers;
 
+use Illuminate\Support\Facades\Http;
+
 class Rocketeers
 {
     protected $client;
     protected $baseUrl;
+    protected $token;
 
     public function __construct($token)
     {
         $this->baseUrl = 'https://rocketeers.app/api/v1';
-
-        $this->client = new \GuzzleHttp\Client([
-            'verify' => false,
-            'timeout' => 3,
-            'headers' => [
-                'Authorization' => 'Bearer '. $token,
-            ]
-        ]);
+        $this->token = $token;
     }
 
     public function report(array $data)
     {
-        return $this->client->post($this->baseUrl.'/errors', $data);
+        return Http::timeout(3)
+            ->withoutVerifying()
+            ->withHeaders([
+                'Authorization' => 'Bearer ' . $this->token,
+            ])
+            ->post($this->baseUrl . '/errors', $data);
     }
 }
