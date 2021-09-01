76c8c6ce7606d8c6ae24d6e811556f8b8f4412a2
Mark van Eijk <mark@vaneijk.co>
2021-09-01 12:02:39 +0200

    check referrer

--- src/Rocketeers.php
+++ src/Rocketeers.php
@@ -18,11 +18,15 @@ class Rocketeers
 
     public function report(array $data)
     {
+        if(isset($_SERVER['HTTP_HOST'])) {
+            $referrer = 'http'.(isset($_SERVER['HTTPS']) ? 's' : '').'://'."{$_SERVER['HTTP_HOST']}/{$_SERVER['REQUEST_URI']}";
+        }
+
         return Http::timeout(3)
             ->withoutVerifying()
             ->withHeaders([
                 'Authorization' => 'Bearer ' . $this->token,
-                'Referer' => 'http'.(isset($_SERVER['HTTPS']) ? 's' : '').'://'."{$_SERVER['HTTP_HOST']}/{$_SERVER['REQUEST_URI']}",
+                'Referer' => $referrer ?? null,
             ])
             ->post($this->baseUrl . '/errors', $data);
     }
